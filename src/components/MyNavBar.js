a741bc33293bddd97698e05c2836db00dc76f134
Gabriel Buchdahl <gbuchdahl@gmail.com>
2019-08-01 22:53:59 -0400

    kept working on navbar

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Image, Col, Container, Row } from "react-bootstrap";
+
+import logo from "../img/bd_logo_2019.png";
 
 export default class MyNavBar extends Component {
   render() {
     return (
       <Navbar expand="lg" bg="dark" fixed="top" variant="dark">
-        <Navbar.Brand href="#home">Bakers Dozen of Yale</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto text-light">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container fluid>
+          <Row className="w-100">
+            <Navbar.Brand className="navbar-brand" href="#home">
+              <Image src={logo} className="ml-3" fluid />
+            </Navbar.Brand>
+            <Navbar.Toggle
+              className="ml-auto"
+              aria-controls="basic-navbar-nav"
+            />
+            <Navbar.Collapse
+              className="justify-content-end"
+              id="responsive-navbar-nav"
+            >
+              <Nav className="text-white text-right">
+                <Nav.Link className="flex-grow-1" href="#link">
+                  LINK
+                </Nav.Link>
+                <Nav.Link className="flex-grow-1" href="#link">
+                  LINK
+                </Nav.Link>
+                <Nav.Link className="flex-grow-1" href="#link">
+                  LINK
+                </Nav.Link>
+                <Nav.Link className="flex-grow-1" href="#link">
+                  LINK
+                </Nav.Link>
+                <Nav.Link className="flex-grow-1" href="#link">
+                  LINK
+                </Nav.Link>
+                <Nav.Link className="flex-grow-1" href="#link">
+                  LINK
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Row>
+        </Container>
       </Navbar>
     );
   }
---
+++ src/stories/MyNavBar.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import MyNavBar from "../components/MyNavBar";
+
+storiesOf("MyNavBar", module).add("default", () => <MyNavBar />);

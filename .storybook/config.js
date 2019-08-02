081af0e5d6534503d62d77dc4ea823b7d23776df
Gabriel Buchdahl <gbuchdahl@gmail.com>
2019-08-01 21:36:43 -0400

    Images and fonts

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from "@storybook/react";
+import "../src/styles/mod_bootstrap.scss";
+
+const req = require.context("../src/stories", true, /\.stories.js$/);
 
 function loadStories() {
-  require("../src/stories");
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/mod_bootstrap.scss";
 import { Container, Row } from "react-bootstrap";
 import MyNavBar from "./components/MyNavBar";
+import Hero from "./components/Hero";
 
 function App() {
   return (

06a4a5eea8b98b5d3b1a43162c5bc0d873b5735b
Gabriel Buchdahl <gbuchdahl@gmail.com>
2019-08-19 23:19:17 -0400

    bulma

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import coverPhoto from "./img/main_photo.jpg";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Hero title="The Baker's Dozen" background={coverPhoto} />
-    </div>
-  );
+  return <h1>Sup</h1>;
 }
 
 export default App;
